e589527d931a3e4ea4836ddc18e5b6aa8438c645
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-01 19:13:37 +0530

    welcome file of user registration

---
+++ UserRegistration.java
@@ -0,0 +1,9 @@
+package com.regestration;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome in User Registration System Applying all Validation!!!!");
+    }
+
+}
